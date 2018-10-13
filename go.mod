12642d85ba8426620a28cdc5480cdb88ab3e0329
rohanthewiz@gmail.com <rohanthewiz@gmail.com>
2018-10-13 15:47:17 -0500

    initial commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module church_example
+
+require (
+	github.com/rohanthewiz/church v0.0.2-0.20181013203239-1f86f8a9fc02
+	github.com/rohanthewiz/logger v1.0.1
+	github.com/rohanthewiz/roredis v0.0.0-20180930023327-1f6380dfc68e
+	github.com/sirupsen/logrus v1.1.1
+	gopkg.in/relistan/rubberneck.v1 v1.1.0
+)
---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+// go:generate sqlboiler postgres
+
+import (
+	"github.com/rohanthewiz/church/config"
+	"github.com/rohanthewiz/church/db"
+	"github.com/rohanthewiz/church"
+	. "github.com/rohanthewiz/logger"
+	"github.com/rohanthewiz/roredis"
+	"github.com/sirupsen/logrus"
+	"gopkg.in/relistan/rubberneck.v1"
+)
+
+var Version, GitCommitHash, BuildTimestamp string  // buildtime vars
+
+func init() {
+	config.InitConfig(Version, GitCommitHash, BuildTimestamp)
+}
+
+func main() {
+	InitLog(LogOptions{
+		AppName: config.APP_NAME,
+		Environment: config.AppEnv,
+		Format: config.Options.Log.Format,
+		Level: config.Options.Log.Level,
+		InfoPath: config.Options.Log.InfoPath,
+		ErrorPath: config.Options.Log.ErrorPath,
+	})
+	defer CloseLog()
+
+	err := db.InitDB(db.DBOpts{
+		DBType: db.DBTypes.Postgres,
+		Host: config.Options.PG.Host,
+		Port: config.Options.PG.Port,
+		User: config.Options.PG.User,
+		Word: config.Options.PG.Word,
+		Database: config.Options.PG.Database,
+	})
+	if err != nil {
+		LogErr(err, "Could not setup database")
+	}
+	defer db.CloseDB()
+
+	roredis.InitRedis(roredis.RedisCfg{
+		Host: "localhost", DB: 0, // default -> Port: "6379",
+	})
+
+	logrus.Println(config.APP_NAME, "is starting...")
+	rubberneck.NewPrinter(logrus.Infof, rubberneck.NoAddLineFeed).Print(config.Options) // print config to log
+
+	Log("Info", "Version Info", "Version", config.Version, "CommitHash", config.GitCommitHash,
+			"BuildTimestamp", config.BuildTimestamp)
+
+	church.Serve()
+}
